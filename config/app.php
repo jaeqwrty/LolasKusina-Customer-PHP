78ffdba03927617b499ff89f655a0a10bbfabf58
Anjelo <anjelovidal28@gmail.com>
2026-03-14 02:04:17 +0800

    Add SMS notifications services with sephamore integrated key

--- config/app.php
+++ config/app.php
@@ -7,6 +7,8 @@
  */
 
 // Database credentials
+define('SEMAPHORE_API_KEY', '282884c7c86f2c80fe3b035e429837c8');
+define('SEMAPHORE_SENDER_NAME', 'LolasKusina'); 
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
